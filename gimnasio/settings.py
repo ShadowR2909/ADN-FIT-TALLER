befc9e8bd7cfac1ec5369fc41932b15d233c4e95
rosario <yanezr267@gmail.com>
2025-10-21 15:05:20 -0300

    Merge rama-gon-editada into development y resolucion de conflicto en settings.py

--- gimnasio/settings.py
+++ gimnasio/settings.py
@@ -128,7 +128,7 @@ DATABASES = {
 #        'USER': 'root',      
 #        'PASSWORD': 'root',  
 #        'HOST': 'localhost',  
-#        'PORT': '3306',            
+#        'PORT': '3306',           
 #    }
 #}
 
